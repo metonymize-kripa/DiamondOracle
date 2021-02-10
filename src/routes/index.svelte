755aa80790207ce495a4297476b8561b9fa0e428
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-10 07:27:55 -0600

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,7 +33,7 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Diamond Oracle</title>
 </svelte:head>
 
 <h1>Great success!</h1>
@@ -43,4 +43,4 @@
 	<figcaption>HIGH FIVE!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><strong>Who is the Oracle? What is the Matrix? The truth is, we no longer know. .</strong></p>

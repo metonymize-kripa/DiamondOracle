e4c000e02abe4599936e9e15d5c5c7ca17384db2
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-10 09:06:57 -0600

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,22 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>About 💎 Oracle</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>Who is the Oracle? What is the Matrix? The truth is, we no longer know. 
+
+What we do know is that in the Matrix, the Oracle tells Neo what he needs to hear. It may not be what he wants to hear, but it is what he needs to hear.
+
+The Diamond Oracle is that, for us. All of us. It is open-sourced. It is free. The 💎 Oracle summarizes publicly available data from the Options market into a tool that answers target price and odds questions based on what the options market collectively implies. Nothing more, nothing less. 
+
+Why? Because we can. 
+
+Diamond Hands will learn to join as a Diamond Mind. We want to be there when it happens.
+</p>
+
+<h3>ORACLE: Sorry kiddo. You got the gift, but it looks like you're waiting for something.</h3>
+<h3>NEO: What?</h3>
+
+<p> The Oracle ...
+<a href="http://www.benzado.com/library/right-as-rain.html">Right as Rain</a>.
+</p>

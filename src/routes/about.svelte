663ed54fe736fa6d954634fe61fbbc0bf5bbdde6
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-10 09:19:19 -0600

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,16 +4,16 @@
 
 <h1>About 💎 Oracle</h1>
 
-<p>Who is the Oracle? What is the Matrix? The truth is, we no longer know. 
+<p>Who is the Oracle? What is the Matrix? The truth is, we no longer know.</p>
 
-What we do know is that in the Matrix, the Oracle tells Neo what he needs to hear. It may not be what he wants to hear, but it is what he needs to hear.
+<p>What we do know is that in the Matrix, the Oracle tells Neo what he needs to hear. It may not be what he wants to hear, but it is what he needs to hear.</p>
 
-The Diamond Oracle is that, for us. All of us. It is open-sourced. It is free. The 💎 Oracle summarizes publicly available data from the Options market into a tool that answers target price and odds questions based on what the options market collectively implies. Nothing more, nothing less. 
+<p>The Diamond Oracle is that, for us. All of us. It is open-sourced. It is free. The 💎 Oracle summarizes publicly available data from the Options market into a tool that answers target price and odds questions based on what the options market collectively implies. Nothing more, nothing less.</p> 
 
-Why? Because we can. 
+<p>Why? Because we can.</p> 
 
-Diamond Hands will learn to join as a Diamond Mind. We want to be there when it happens.
-</p>
+<p>Diamond Hands will learn to join as a Diamond Mind. We want to be there when it happens...</p>
+
 
 <h3>ORACLE: Sorry kiddo. You got the gift, but it looks like you're waiting for something.</h3>
 <h3>NEO: What?</h3>

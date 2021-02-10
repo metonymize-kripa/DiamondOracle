8c5d30da949d9e7aa6eedadcfb145cfb17e25b70
goindi <45494818+goindi@users.noreply.github.com>
2021-02-10 11:09:38 -0500

    Update _posts.js

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -16,11 +16,11 @@ const posts = [
 		`,
   },
 	   {
-    title: 'How does the Oracle know?',
+    title: 'How does the 💎 Oracle know?',
     slug: 'how-does-the-oracle-know',
     html: `
-	<p>We're so glad you asked! Oracle, as we know, is a just a <a href='https://github.com/metonymize-kripa/DiamondOracle/blob/master/src/diamond_hand_index.py'>very smart and kind program</a>. 
- 	It uses publicly available data from Google Finance, Yahoo Finance, python package (<a href='https://github.com/mcdallas/wallstreet'> wallstreet </a>) to calculate probabilities implied in option pricing. </p>
+	<p>We're so glad you asked! 💎 Oracle, as we know, is a just a <a href='https://github.com/metonymize-kripa/DiamondOracle/blob/master/src/diamond_hand_index.py'>very smart and kind program</a>. 
+ 	It uses publicly available data from Google Finance, Yahoo Finance, python package <a href='https://github.com/mcdallas/wallstreet'> wallstreet</a>) to calculate probabilities implied in option pricing. </p>
 		`,
   },
 	  {

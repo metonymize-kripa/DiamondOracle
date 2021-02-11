85e06ddee794d086090c91423cfcef2cae6741d6
Harmeet Goindi <goindi@gmail.com>
2021-02-10 20:48:44 -0500

    added symbols.txt to have symbols to run 1st term is days. default is 5 if no days entered this generated index.svlete file

--- src/routes/diamond_hand_index.py
+++ src/routes/diamond_hand_index.py
@@ -4,7 +4,7 @@ import numpy as np
 from datetime import datetime, timedelta
 from wallstreet import Call, Put, Stock
 from scipy.stats import norm
-
+from datetime import datetime, timedelta
 
 def get_expiries_bracket(ticker, num_of_days):
     c = Call(ticker)
@@ -53,7 +53,6 @@ def get_strike_bracket(call_object, price_target):
 def get_atm_ivol(s, ndays=30):
     symbol = s.ticker
     expiry_dict = get_expiries_bracket(symbol, ndays)
-
     #First Shorter One
     x = expiry_dict['shorter_expiry']
     shorter_call = Call(symbol,d=int(x[0:2]),m=int(x[3:5]),y=int(x[6:10]))
@@ -76,17 +75,94 @@ def get_atm_ivol(s, ndays=30):
     one_sigma_move_ndays_day = implied_ivol*math.sqrt(ndays/365)
     return (implied_ivol, one_sigma_move_ndays_day)
 
+def generate_svelte_file():
+    timestamp = datetime.now().strftime('%b,%d %Y %I:%M:%S %p')
+    svelte_file = """
+    <style>
+    	h1, figure, p {
+    		text-align: center;
+    		margin: 0 auto;
+    	}
+
+    	h1 {
+    		font-size: 2.8em;
+    		text-transform: uppercase;
+    		font-weight: 700;
+    		margin: 0 0 0.5em 0;
+    	}
+
+    	figure {
+    		margin: 0 0 1em 0;
+    	}
+
+    	img {
+    		width: 100%;
+    		max-width: 400px;
+    		margin: 0 0 1em 0;
+    	}
+
+    	p {
+    		margin: 1em auto;
+    	}
+
+    	@media (min-width: 480px) {
+    		h1 {
+    			font-size: 4em;
+    		}
+    	}
+    </style>
+
+    <svelte:head>
+    	<title>Diamond Oracle</title>
+    </svelte:head>
+
+    <h1>💎 Oracle</h1>
+    """
+    file_to_append = f"{svelte_file}<p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled {timestamp}</em></p>"
+
+    with open("symbols.txt",'r') as f:
+        sym_list = f.read().replace(" ", "").replace("\n", "").replace("\t", "").split(',')
+        n_days = 5
+        try:
+            n_days = int(sym_list[0])
+            sym_list = sym_list[1:]
+        except ValueError:
+            print("expected a number. Defaulting to 5")
+        td = timedelta(n_days)
+        print(sym_list)
+        for i in sym_list:
+            s = Stock(i)
+            my_tuple = get_atm_ivol(s, n_days)
+            my_range = my_tuple[1]*1.96*s.price
+            file_to_append = f"{file_to_append}<p><strong>{s.ticker}: ${round(s.price - my_range)} - ${round(s.price + my_range)}</strong></p>\n"
+
+        file_to_append = f"""
+        {file_to_append}<p>[/🔱 Target Price: {(datetime.now()+td).strftime('%b,%d %Y')}, Odds: 95%]</p>
+
+        <figure>
+        	<img alt='Borat' src='great-success.png'>
+        	<figcaption>HIGH FIVE!</figcaption>
+        </figure>
+
+        <p>Pappe: Who is the Oracle? What is the Matrix? The truth is, we no longer know.</p>
+        """
+
+    with open("../routes/index.svelte",'w') as f:
+       f.write(file_to_append)
+
 
 if __name__== "__main__":
-
     print(len(sys.argv))
     n_days = 30
-    if len(sys.argv) == 3:
-        n_days = int(sys.argv[2])
-    s = Stock(sys.argv[1])
-    my_tuple = get_atm_ivol(s, n_days)
-    big_loss= 0.9
-    sd_move_for_big_loss = big_loss/my_tuple[0]
-    prob_of_loss_ndays = 1 - norm.cdf(sd_move_for_big_loss)
-    print(f"The Diamond Hand Index of {sys.argv[1]} is {prob_of_loss_ndays*100:.7f} ")
-    print(f"{s.ticker} (${s.price}): {n_days} day price range - ${s.price-my_tuple[1]*1.96*s.price:.2f} - ${s.price+my_tuple[1]*1.96*s.price:.2f}")
+    if len(sys.argv) == 1:
+        generate_svelte_file()
+    else:
+        if len(sys.argv) == 3:
+            n_days = int(sys.argv[2])
+        s = Stock(sys.argv[1])
+        my_tuple = get_atm_ivol(s, n_days)
+        big_loss= 0.9
+        sd_move_for_big_loss = big_loss/my_tuple[0]
+        prob_of_loss_ndays = 1 - norm.cdf(sd_move_for_big_loss)
+        print(f"The Diamond Hand Index of {sys.argv[1]} is {prob_of_loss_ndays*100:.7f} ")
+        print(f"{s.ticker} (${s.price}): {n_days} day price range - ${s.price-my_tuple[1]*1.96*s.price:.2f} - ${s.price+my_tuple[1]*1.96*s.price:.2f}")
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,56 +1,52 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+        
+    <style>
+    	h1, figure, p {
+    		text-align: center;
+    		margin: 0 auto;
+    	}
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+    	h1 {
+    		font-size: 2.8em;
+    		text-transform: uppercase;
+    		font-weight: 700;
+    		margin: 0 0 0.5em 0;
+    	}
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+    	figure {
+    		margin: 0 0 1em 0;
+    	}
 
-	p {
-		margin: 1em auto;
-	}
+    	img {
+    		width: 100%;
+    		max-width: 400px;
+    		margin: 0 0 1em 0;
+    	}
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
+    	p {
+    		margin: 1em auto;
+    	}
 
-<svelte:head>
-	<title>Diamond Oracle</title>
-</svelte:head>
+    	@media (min-width: 480px) {
+    		h1 {
+    			font-size: 4em;
+    		}
+    	}
+    </style>
 
-<h1>💎 Oracle</h1>
-<p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled 11:16am EST, Feb-10-2021</em></p>
-<p><strong>GME: $34-72 </strong></p>
-<p><strong>AMC: $4-8 </strong></p>
-<p><strong>PLTR: $29-44 </strong></p>
-<p><strong>BB: $10-16 </strong></p>
-<p><strong>TSLA: $724-915 </strong></p>
+    <svelte:head>
+    	<title>Diamond Oracle</title>
+    </svelte:head>
 
-<p>[/🔱 Target Price: Feb-12-2021, Odds: 95%]</p>
+    <h1>💎 Oracle</h1>
+    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Feb,10 2021 08:48:24 PM</em></p><p><strong>IBM: $110 - $134</strong></p>
+<p><strong>C: $55 - $72</strong></p>
+<p>[/🔱 Target Price: Feb,26 2021, Odds: 95%]</p>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
+        <figure>
+        	<img alt='Borat' src='great-success.png'>
+        	<figcaption>HIGH FIVE!</figcaption>
+        </figure>
 
-<p>Pappe: Who is the Oracle? What is the Matrix? The truth is, we no longer know.</p>
-
-
+        <p>Pappe: Who is the Oracle? What is the Matrix? The truth is, we no longer know.</p>
+        

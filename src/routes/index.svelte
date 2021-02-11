22bb6b7439e55a7f9da509e380ea0316ef4e6225
metonymize-kripa <kripa@metonymize.com>
2021-02-11 12:04:57 -0600

    Heartbeat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,11 +39,11 @@
     </svelte:head>
 
     <h1>💎 Oracle</h1>
-    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Feb,11 2021 11:04:17 AM</em></p><p><strong>GME: $16 - $81</strong></p>
-<p><strong>PLTR: $19 - $50</strong></p>
-<p><strong>BB: $8 - $17</strong></p>
+    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Feb,11 2021 12:04:40 PM</em></p><p><strong>GME: $20 - $78</strong></p>
+<p><strong>PLTR: $20 - $48</strong></p>
+<p><strong>BB: $9 - $16</strong></p>
 <p><strong>AMC: $3 - $8</strong></p>
-<p><strong>TSLA: $682 - $943</strong></p>
+<p><strong>TSLA: $682 - $935</strong></p>
 <p>[/🔱 Target Price: Feb,18 2021, Odds: 95%]</p>
 
         <figure>

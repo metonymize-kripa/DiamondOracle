17c735b5e5a12ff77fdbee41a7c3d92ac293fd66
metonymize-kripa <kripa@metonymize.com>
2021-02-15 22:44:06 -0600

    Heartbeat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,7 +39,7 @@
     </svelte:head>
 
     <h1>💎 Oracle</h1>
-    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Feb,15 2021 09:43:19 PM</em></p><p><strong>GME: $21 - $84</strong></p>
+    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Feb,15 2021 10:43:45 PM</em></p><p><strong>GME: $21 - $84</strong></p>
 <p><strong>PLTR: $15 - $49</strong></p>
 <p><strong>TSLA: $681 - $951</strong></p>
 <p><strong>SPY: $377 - $409</strong></p>

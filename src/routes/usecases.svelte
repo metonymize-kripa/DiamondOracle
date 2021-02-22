ab17a070a5e9c1cbffd83afd9eefb0010978df17
metonymize-kripa <kripa@metonymize.com>
2021-02-22 17:00:34 -0600

    updated use cases text

--- src/routes/usecases.svelte
+++ src/routes/usecases.svelte
@@ -4,6 +4,10 @@
 
 <h1>Some of our use cases</h1>
 
-<h2>Avoiding melt down</h2>
+<p>Options are just one of the many sources of insights that we make available for traders.
+Here are a few use cases to inspire your exploration with our tools. Be bold. Hope you have fun!</p>
+
+
+<h2><strong>Avoiding melt down</strong></h2>
 <p>Using TICKER DOOM allows us to estimate the options implied probability of a sharp near term drop.</p>
 

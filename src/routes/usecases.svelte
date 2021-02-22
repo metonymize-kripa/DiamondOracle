585aeb87955d53fbcb0ce9ce0b2edf691dcd5cd2
metonymize-kripa <kripa@metonymize.com>
2021-02-22 16:50:34 -0600

    added use cases

--- src/routes/usecases.svelte
+++ src/routes/usecases.svelte
@@ -5,5 +5,5 @@
 <h1>Some of our use cases</h1>
 
 <h2>Avoiding melt down</h2>
-<p>Using <TICKER> DOOM allows us to estimate the options implied probability of a sharp near term drop.</p>
+<p>Using TICKER DOOM allows us to estimate the options implied probability of a sharp near term drop.</p>
 

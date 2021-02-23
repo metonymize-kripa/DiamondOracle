fcdb98b4c3f72aaadbdf02ce528fedc7abd84a07
metonymize-kripa <kripa@metonymize.com>
2021-02-23 09:15:30 -0600

    Heartbeat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,16 +39,16 @@
     </svelte:head>
 
     <h1>💎 Oracle</h1>
-    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Feb,23 2021 08:14:02 AM</em></p><p><strong>TSLA: $562 - $867</strong></p>
-<p><strong>AMC: $2 - $11</strong></p>
-<p><strong>PLTR: $18 - $38</strong></p>
-<p><strong>GME: $18 - $74</strong></p>
-<p><strong>SPY: $368 - $406</strong></p>
-<p><strong>LBRDK: $136 - $158</strong></p>
-<p><strong>IAC: $207 - $293</strong></p>
-<p><strong>TWTR: $55 - $86</strong></p>
-<p><strong>SE: $197 - $309</strong></p>
-<p><strong>MELI: $1249 - $2329</strong></p>
+    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Feb,23 2021 09:14:38 AM</em></p><p><strong>TSLA: $546 - $797</strong></p>
+<p><strong>AMC: $3 - $11</strong></p>
+<p><strong>PLTR: $18 - $34</strong></p>
+<p><strong>GME: $18 - $67</strong></p>
+<p><strong>SPY: $365 - $402</strong></p>
+<p><strong>LBRDK: $132 - $159</strong></p>
+<p><strong>IAC: $175 - $309</strong></p>
+<p><strong>TWTR: $53 - $85</strong></p>
+<p><strong>SE: $143 - $351</strong></p>
+<p><strong>MELI: $698 - $2702</strong></p>
 <p>[/🔱 Target Price: Mar,02 2021, Odds: 95%]</p>
 
         <figure>

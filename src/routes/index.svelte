7c51ebd19066b603290f39c3cb33cfb5dd4d86e4
metonymize-kripa <kripa@metonymize.com>
2021-03-02 08:30:54 -0600

    Heartbeat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,20 +39,20 @@
     </svelte:head>
 
     <h1>💎 Oracle</h1>
-    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,02 2021 07:28:47 AM</em></p><p><strong>RKT: $16 - $33</strong></p>
+    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,02 2021 08:29:46 AM</em></p><p><strong>RKT: $16 - $33</strong></p>
 <p><strong>GME: $-40 - $281</strong></p>
 <p><strong>AMC: $2 - $17</strong></p>
 <p><strong>PLTR: $18 - $31</strong></p>
 <p><strong>NIO: $31 - $69</strong></p>
-<p><strong>SPY: $369 - $410</strong></p>
+<p><strong>SPY: $371 - $409</strong></p>
 <p><strong>TSLA: $586 - $851</strong></p>
-<p><strong>AAPL: $115 - $140</strong></p>
+<p><strong>AAPL: $118 - $139</strong></p>
 <p><strong>BB: $7 - $15</strong></p>
-<p><strong>SOS: $1 - $13</strong></p>
+<p><strong>SOS: $4 - $12</strong></p>
 <p><strong>SNDL: $1 - $2</strong></p>
-<p><strong>ZM: $281 - $539</strong></p>
-<p><strong>AMD: $76 - $97</strong></p>
-<p><strong>SQ: $201 - $281</strong></p>
+<p><strong>ZM: $360 - $514</strong></p>
+<p><strong>AMD: $77 - $97</strong></p>
+<p><strong>SQ: $235 - $269</strong></p>
 <p>[/🔱 Target Price: Mar,09 2021, Odds: 95%]</p>
 
         <figure>

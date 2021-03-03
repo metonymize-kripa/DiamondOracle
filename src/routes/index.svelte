22c9d587bb78b38795d46087e723c059f76f1d97
metonymize-kripa <kripa@metonymize.com>
2021-03-03 08:37:56 -0600

    Heartbeat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,18 +39,18 @@
     </svelte:head>
 
     <h1>💎 Oracle</h1>
-    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,03 2021 07:36:34 AM</em></p><p><strong>GME: $-7 - $244</strong></p>
-<p><strong>UWMC: $6 - $12</strong></p>
+    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,03 2021 08:37:15 AM</em></p><p><strong>GME: $8 - $238</strong></p>
+<p><strong>UWMC: $9 - $13</strong></p>
 <p><strong>AMC: $3 - $15</strong></p>
-<p><strong>PLTR: $18 - $31</strong></p>
-<p><strong>NIO: $30 - $56</strong></p>
-<p><strong>TSLA: $547 - $826</strong></p>
-<p><strong>SPY: $364 - $410</strong></p>
-<p><strong>FUBO: $16 - $67</strong></p>
-<p><strong>KMPH: $3 - $16</strong></p>
+<p><strong>PLTR: $20 - $30</strong></p>
+<p><strong>NIO: $34 - $55</strong></p>
+<p><strong>TSLA: $544 - $827</strong></p>
+<p><strong>SPY: $361 - $411</strong></p>
+<p><strong>FUBO: $-2 - $75</strong></p>
+<p><strong>KMPH: $14 - $20</strong></p>
 <p><strong>SNDL: $0 - $2</strong></p>
 <p><strong>BB: $7 - $14</strong></p>
-<p><strong>TLRY: $14 - $38</strong></p>
+<p><strong>TLRY: $15 - $37</strong></p>
 <p><strong>AAPL: $112 - $138</strong></p>
 <p>[/🔱 Target Price: Mar,10 2021, Odds: 95%]</p>
 

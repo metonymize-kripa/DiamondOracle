d68d8458a9fb8cf0b30d15e2dcfdcc5d276a1a98
metonymize-kripa <kripa@metonymize.com>
2021-03-04 13:59:13 -0600

    Heartbeat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,20 +39,20 @@
     </svelte:head>
 
     <h1>💎 Oracle</h1>
-    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,04 2021 12:57:48 PM</em></p><p><strong>GME: $19 - $221</strong></p>
-<p><strong>UWMC: $5 - $12</strong></p>
-<p><strong>PLTR: $14 - $33</strong></p>
-<p><strong>RKT: $13 - $39</strong></p>
-<p><strong>SKT: $9 - $24</strong></p>
-<p><strong>SPY: $333 - $412</strong></p>
-<p><strong>TSLA: $402 - $805</strong></p>
-<p><strong>SOS: $1 - $12</strong></p>
-<p><strong>AMC: $4 - $12</strong></p>
-<p><strong>NIO: $21 - $55</strong></p>
-<p><strong>QQQ: $254 - $348</strong></p>
-<p><strong>AAPL: $100 - $138</strong></p>
-<p><strong>AMD: $62 - $92</strong></p>
-<p><strong>ARKK: $80 - $151</strong></p>
+    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,04 2021 01:58:34 PM</em></p><p><strong>GME: $-10 - $273</strong></p>
+<p><strong>UWMC: $6 - $12</strong></p>
+<p><strong>PLTR: $18 - $31</strong></p>
+<p><strong>RKT: $16 - $37</strong></p>
+<p><strong>SKT: $12 - $23</strong></p>
+<p><strong>SPY: $355 - $399</strong></p>
+<p><strong>TSLA: $490 - $754</strong></p>
+<p><strong>SOS: $2 - $12</strong></p>
+<p><strong>AMC: $4 - $13</strong></p>
+<p><strong>NIO: $28 - $51</strong></p>
+<p><strong>QQQ: $276 - $334</strong></p>
+<p><strong>AAPL: $105 - $135</strong></p>
+<p><strong>AMD: $68 - $89</strong></p>
+<p><strong>ARKK: $96 - $142</strong></p>
 <p>[/🔱 Target Price: Mar,11 2021, Odds: 95%]</p>
 
         <figure>

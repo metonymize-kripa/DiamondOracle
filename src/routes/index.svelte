84a852fd8ac56eb43e430c8a326316bb3c3a5022
metonymize-kripa <kripa@metonymize.com>
2021-03-05 13:15:59 -0600

    Heartbeat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,18 +39,18 @@
     </svelte:head>
 
     <h1>💎 Oracle</h1>
-    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,05 2021 12:14:20 PM</em></p><p><strong>GME: $30 - $248</strong></p>
-<p><strong>SPY: $354 - $405</strong></p>
+    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,05 2021 01:15:06 PM</em></p><p><strong>GME: $30 - $253</strong></p>
+<p><strong>SPY: $357 - $405</strong></p>
 <p><strong>PLTR: $16 - $30</strong></p>
-<p><strong>TSLA: $424 - $749</strong></p>
-<p><strong>RKT: $18 - $32</strong></p>
+<p><strong>TSLA: $416 - $764</strong></p>
+<p><strong>RKT: $18 - $33</strong></p>
 <p><strong>UWMC: $5 - $12</strong></p>
 <p><strong>AMC: $4 - $12</strong></p>
-<p><strong>AAPL: $107 - $132</strong></p>
+<p><strong>AAPL: $107 - $133</strong></p>
 <p><strong>AMD: $72 - $84</strong></p>
-<p><strong>SKT: $11 - $21</strong></p>
-<p><strong>QQQ: $277 - $334</strong></p>
-<p><strong>ARKK: $92 - $137</strong></p>
+<p><strong>SKT: $11 - $22</strong></p>
+<p><strong>QQQ: $278 - $335</strong></p>
+<p><strong>ARKK: $92 - $138</strong></p>
 <p><strong>NIO: $28 - $45</strong></p>
 <p><strong>SQQQ: $11 - $20</strong></p>
 <p>[/🔱 Target Price: Mar,12 2021, Odds: 95%]</p>

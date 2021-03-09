494855064a2a5a5b8697129d045b92a699bb525a
metonymize-kripa <kripa@metonymize.com>
2021-03-09 13:24:16 -0600

    Heartbeat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,21 +39,21 @@
     </svelte:head>
 
     <h1>💎 Oracle</h1>
-    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,09 2021 12:22:51 PM</em></p><p><strong>LBRDK: $139 - $165</strong></p>
-<p><strong>IAC: $218 - $256</strong></p>
-<p><strong>TWTR: $58 - $77</strong></p>
-<p><strong>SE: $192 - $265</strong></p>
-<p><strong>MELI: $1228 - $1671</strong></p>
-<p><strong>PYPL: $214 - $272</strong></p>
-<p><strong>PTON: $94 - $138</strong></p>
-<p><strong>CHNG: $17 - $29</strong></p>
-<p><strong>CVNA: $206 - $314</strong></p>
-<p><strong>MU: $78 - $100</strong></p>
-<p><strong>PINS: $55 - $80</strong></p>
-<p><strong>DIS: $181 - $214</strong></p>
+    <p><em>Using <a href='https://finance.yahoo.com/quote/GME/options?p=GME'>Yahoo Finance Options Data</a>; Pulled Mar,09 2021 01:23:36 PM</em></p><p><strong>LBRDK: $135 - $167</strong></p>
+<p><strong>IAC: $218 - $257</strong></p>
+<p><strong>TWTR: $56 - $79</strong></p>
+<p><strong>SE: $178 - $275</strong></p>
+<p><strong>MELI: $1189 - $1690</strong></p>
+<p><strong>PYPL: $207 - $279</strong></p>
+<p><strong>PTON: $88 - $142</strong></p>
+<p><strong>CHNG: $16 - $29</strong></p>
+<p><strong>CVNA: $203 - $315</strong></p>
+<p><strong>MU: $76 - $103</strong></p>
+<p><strong>PINS: $53 - $81</strong></p>
+<p><strong>DIS: $174 - $218</strong></p>
 <p><strong>WFC: $34 - $43</strong></p>
-<p><strong>C: $63 - $80</strong></p>
-<p><strong>GDDY: $68 - $83</strong></p>
+<p><strong>C: $64 - $80</strong></p>
+<p><strong>GDDY: $66 - $84</strong></p>
 <p>[/🔱 Target Price: Mar,16 2021, Odds: 95%]</p>
 
         <figure>
